7b60c3c6c5123f35a41b6a565a29987de3489ed8
StoyanovKS <stoyanov.krasen@yahoo.com>
2026-01-25 21:16:24 +0200

    templates_p10

--- core/urls.py
+++ core/urls.py
@@ -4,4 +4,11 @@ from .views import HomeView, DashboardView
 urlpatterns = [
     path("", HomeView.as_view(), name="home"),
     path("dashboard/", DashboardView.as_view(), name="dashboard"),
+    path("about/", TemplateView.as_view(template_name="core/about.html"), name="about"),
+    path("features/", TemplateView.as_view(template_name="core/features.html"), name="features"),
+    path("demo/", TemplateView.as_view(template_name="core/demo.html"), name="demo"),
 ]
+
+
+
+
