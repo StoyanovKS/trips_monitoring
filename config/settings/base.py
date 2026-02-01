24b644232c4a9ba41c3fa1fae50c703c67801c35
StoyanovKS <stoyanov.krasen@yahoo.com>
2026-02-01 17:45:08 +0200

    updated_base.py_dbs

--- config/settings/base.py
+++ config/settings/base.py
@@ -74,14 +74,19 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # DATABASE (PostgreSQL)
 # ==============================================================================
 
+
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.getenv('DB_NAME'),
-        'USER': os.getenv('DB_USER'),
-        'PASSWORD': os.getenv('DB_PASSWORD'),
-        'HOST': os.getenv('DB_HOST', 'localhost'),
-        'PORT': os.getenv('DB_PORT', '5432'),
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": os.environ.get("DB_NAME"),
+        "USER": os.environ.get("DB_USER"),
+        "PASSWORD": os.environ.get("DB_PASSWORD"),
+        "HOST": os.getenv('DB_HOST', 'localhost'),
+        "PORT": os.environ.get("DB_PORT", "5432"),
+        "OPTIONS": {
+            "sslmode": "require",
+        },
     }
 }
 
